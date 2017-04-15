eceb755bd5401a6afb0027edc061f3b2254f976f
Ji Xiang <jx4554@gmail.com>
2017-04-15 14:56:13 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
   
   root 'jobs#index'
