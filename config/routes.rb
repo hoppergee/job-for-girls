f2001dbdb06ad3309034094f45a85e609e10ffc7
Ji Xiang <jx4554@gmail.com>
2017-04-18 08:19:49 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      
+      resources :resumes
     end
   end
   
-  root 'jobs#index'
+  root 'welcome#index'
 end
