02055199df91dab5fbe3f04a12c67d3e4f4ea231
Ji Xiang <jx4554@gmail.com>
2017-04-22 17:32:27 +0800

    going finish search bar

--- app/assets/javascripts/jobs_common.js
+++ app/assets/javascripts/jobs_common.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-	$("input").focus(function(){
-		$(this).css("background-color", "#cccccc");
-	    $("#DropDown_br").show();
+	$('input[id^="SearchInput__"]').focus(function(){
+		// $(this).css("background-color", "#cccccc");
+	    $(".SearchDropDownWrapper").show();
 	});
 	// $("input").blur(function(){
 	// 	$(this).css("background-color", "#ffffff");
@@ -9,9 +9,23 @@ $(document).ready(function(){
 	// });
 	
 	$(".Destination").click(function(){
-		$(this).css("background-color", "#ffffff");
-	    $("#DropDown_br").hide();
+		// $(this).css("background-color", "#ffffff");
+		// console.log('ddddddd', $(this).value())
+		console.log("dddddddddddd");
+		console.log($(this).text());
+		console.log($("#LocationSearch_input").attr("placeholder"));
+		$("#SearchInput__city:text").val($(this).text())
+		// $("#LocationSearch_input").value("nice");
+	    $(".SearchDropDownWrapper").hide();
 	});
+	
+		//     $("div").click(function(){
+		// var thisone = document.activeElement;
+		// var thatone = document.getElementsByClassName("SearchContainer")
+		//         if(!($.contains(thatone, thisone))) {
+		//         	$(".SearchDropDownWrapper").hide();
+		//         }
+		//     });
 });
 
 
